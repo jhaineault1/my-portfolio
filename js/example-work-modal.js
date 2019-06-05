08f6da3474634c86a4e56ec64024ffa6205af613
jhaineault1 <jhaineault1@gmail.com>
2019-06-05 18:40:27 -0400

    added js files and folder

--- js/example-work-modal.js
+++ js/example-work-modal.js
@@ -30,5 +30,4 @@ class ExampleWorkModal extends React.Component {
     )
   };
 };
-
 export default ExampleWorkModal;
--- js/main.js
+++ js/main.js
@@ -44,4 +44,5 @@ const myWork = [
     }
   }
 ]
+
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
