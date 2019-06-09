c25285e8b24e7c02a0c990ee9ffba5328580c71b
jhaineault1 <jhaineault1@gmail.com>
2019-06-09 11:49:28 -0400

    Edited AboutMe section

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const myWork = [
   {
     'title': "Python Projects",
     'href': "http://python-automation.jeffhaineault.info/",
-    'desc': "",
+    'desc': "This python script syncs a local directory to an AWS S3 bucket and also helps manage DNS and AWS CloudFront(AWS's content delivery network). A single command publishes this website from a local machine to an AWS S3 bucket." ,
     'image': {
       'desc': "example screenshot of a project involving code",
       'src': "images/example7.png",
@@ -16,7 +16,7 @@ const myWork = [
   {
     'title': "Kubernetes Projects",
     'href': "images/giphy.gif",
-    'desc': "",
+    'desc': "I am currently in the process of moving this site into Kubernetes containers.",
     'image': {
       'desc': "Serverless Portfolio",
       'src': "images/example6.png",
@@ -26,7 +26,7 @@ const myWork = [
   {
     'title': "Hobbies",
     'href': "https://www.reddit.com/r/fightporn/comments/bwf1yz/things_escalate_quickly_when_a_guy_gets_his_bike/?utm_source=share&utm_medium=web2x",
-    'desc': "",
+    'desc': "I live to hike, camp, ride bikes and explore. Here's a collection of hikes and camps we've done over the years.",
     'image': {
       'desc': "Serverless Portfolio",
       'src': "images/example5.jpg",
@@ -36,7 +36,7 @@ const myWork = [
   {
     'title': "About Me",
     'href': "https://example.com",
-    'desc': "",
+    'desc': "I grew up on a farm down a dirt road in rural south Georgia where I spent most of my childhood learning how to raise various farm animals and exploring outside. As a young teenager I became fascinated with music and technology which led me to playing drums and conquering video games. This passion later led me to pursue a career as a touring musician in my mid 20's and graduate with a Bachelors in Computer Information Systems. I have passionately been pursuing a career in technology ever since and are currently studying to become a Site Reliability Engineer. As of 2019 I have 4 years experience as an IT technician/Jr. Sys Admin.",
     'image': {
       'desc': "example screenshot",
       'src': "images/example4.jpg",
