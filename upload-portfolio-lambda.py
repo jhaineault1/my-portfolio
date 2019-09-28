8a6432550b40e1257c97c0fc5ce8c722ed27c391
jhaineault1 <jhaineault1@gmail.com>
2019-09-28 16:23:15 -0400

    updated CodePipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
         print "Job Done!"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
         if job:
-            codepipeline = boto3.client('codepipeline')
+            codepipeline = boto3.client('CodePipeline')
             codepipeline.put_job_success_result(jobId=job["id"])
 
     except:
